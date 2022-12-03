76400a4604c3208c1d382ee04e666dab752a4c04
Helen Lehrer <hlehrer@outlook.com>
2022-12-02 16:56:59 -0800

    Fix spacing

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import './Navbar.scss';
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
