62af0093f279025879dcdfb65b8df342dfa47813
Helen Lehrer <hlehrer@outlook.com>
2022-12-09 12:05:03 -0800

    WIP Work.jsx

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -9,13 +9,16 @@ import './Work.scss';
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState('All');
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 })
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "works"]';
 
     client.fetch(query)
       .then((data) => {
-        setWorksds(data);
+        setWorks(data);
+        setFilterWork(data);
       })
   }, [])
   
@@ -43,7 +46,15 @@ const Work = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__work-portfolio"
       >
-        
+        {filterWork.map((work, index) => (
+          <div 
+          className="app__work-item app__flex"
+          key={index}>
+            <div className="app__work-img app__flex">
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+            </div>
+          </div>
+        ))}
       </motion.div>
     </>
   )
