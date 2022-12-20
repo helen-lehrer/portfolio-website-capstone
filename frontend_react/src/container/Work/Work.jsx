f5af28792fe7ce7dc2b8bc4703522e2928cd5533
Helen Lehrer <hlehrer@outlook.com>
2022-12-19 16:49:29 -0800

    Add entry to Research & Planning Log

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {['ASP.NET', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['React JS', 'ASP.NET C#', 'All'].map((item, index) => (
           <div
           key={index}
           onClick={() => handleWorkFilter(item)}
