32a4eef89a24a1780d3063a5a0ef360b1fbae49d
Helen Lehrer <hlehrer@outlook.com>
2022-12-21 09:00:47 -0800

    WIP add react-router and CaseStudy component

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 import { About, Footer, Header, Skills, Work } from './container';
 import { Navbar } from './components';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.scss';
 
 const App = () => {
   return (
     <div className="app">
-      <Navbar />
-      <Header />
-      <About />
-      <Work />
-      <Skills />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Footer />
+        <Routes>
+          
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ frontend_react/src/container/CaseStudy/CaseStudy.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
+
+import './CaseStudy.scss';
+
+const CaseStudy = () => {
+  return (
+    <>
+      <h1>Case Study</h1>
+    </>
+  )
+}
+
+export default CaseStudy
