bb5f222a279db61964be12de9cf73fcda4c6efd6
Helen Lehrer <hlehrer@outlook.com>
2022-12-22 10:46:56 -0800

    Add placeholder info for CaseStudy

--- frontend_react/src/container/CaseStudy/CaseStudy.jsx
+++ frontend_react/src/container/CaseStudy/CaseStudy.jsx
@@ -15,6 +15,8 @@ const CaseStudy = (props) => {
         <div className="app__casestudy-img">
           <img src={urlFor(work.imgUrl)} alt={work.name} />
         </div>
+        <b className="p-text app__flex app__casestudy-desc">(in progress)</b>
+        <p className="p-text app__flex app__casestudy-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </>
   )
