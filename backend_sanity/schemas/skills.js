02772f8d88456fe57230bc1caa8d173c263393d5
Helen Lehrer <hlehrer@outlook.com>
2023-06-11 17:21:37 -0700

    Add sort # to skills schema

--- backend_sanity/schemas/skills.js
+++ backend_sanity/schemas/skills.js
@@ -21,6 +21,10 @@ export default{
               hotspot: true,
             },
         },
-        
+        {
+            name: 'sort',
+            title: 'Sort #',
+            type: 'number'
+        }
     ]
 }
